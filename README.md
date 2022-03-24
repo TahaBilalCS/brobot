9f3ed49e2127b2801f362b3809d146f374e6fd47
TahaBilalCS <bilaltahacs@gmail.com>
2022-03-24 17:47:56 -0400

    Update line ending formatting to manage different OS environments
    - Install exact version of prettier for more consistent formatting between devs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Brobot
+
 AWS EC2 instance hosting an https and websocket server through express using Typescript.
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,13 +6,13 @@ module.exports = {
         {
             name: 'brobot',
             script: 'dist/src/index.js',
-            node_args: "--experimental-modules --es-module-specifier-resolution=node", // So we can remove ".js" extensions from imports
+            node_args: '--experimental-modules --es-module-specifier-resolution=node', // So we can remove ".js" extensions from imports
             // args: "",
             // watch: ["src"],
             // watch_delay: 1000,
             // ignore_watch: ["node_modules", "dist", "__tests__"],
             env: {
-                NODE_ENV: "development",
+                NODE_ENV: 'development'
                 // Add env vars here if you want them to update on restart. Can just kill the daemon
             }
         }
@@ -25,15 +25,14 @@ module.exports = {
             ref: 'origin/main',
             repo: 'git@github.com:TahaBilalCS/brobot.git',
             path: process.env.AWS_EC2_PATH,
-            node_args: "--experimental-modules --es-module-specifier-resolution=node",
+            node_args: '--experimental-modules --es-module-specifier-resolution=node',
             env: {
-                NODE_ENV: 'production',
+                NODE_ENV: 'production'
                 // update environment variables in /etc/environment on EC2 instance. pm2 ain't working correctly
                 // pm2 caches environment variables and --update-env [--env aws] doesn't seem to be working
                 // Need to delete pm2 instance before starting instance again to update
             },
-            'post-deploy':
-                'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env production'
+            'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env production'
         }
     }
 };
