d3735531f5a9a4332b42799911dda5b9c830178e
brotherbill <bilaltahacs@gmail.com>
2022-08-11 01:16:57 -0400

    Wipe Old Project

--- src/utils/TimeUtil.ts
+++
@@ -1,8 +0,0 @@
-import moment from 'moment-timezone';
-
-/**
- * Get current date and return in EST string format
- */
-export const getCurrentDateEST = (): string => {
-    return moment.tz(Date.now(), 'America/New_York').format('LLL');
-};
