f21aa2d29ccf89a1c95136a05a2ede0752fd6c23
brotherbill <bilaltahacs@gmail.com>
2022-08-13 23:42:29 -0400

    Wipe Old Project

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+
 const app = express();
+const expressWs = require('express-ws')(app)
+
 app.get("/", (req, res) => {
   res.send("LET'S DO THIS");
 });
+
+app.ws('/ashketchum', (ws, req) => {
+  console.log("SOMEONE HERE")
+  ws.on('message', (msg) => {
+    console.log(msg)
+  })
+})
+
 app.listen(3000, () => console.log("Server running on port 3000"));
