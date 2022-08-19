b05b2e5b535aa09bf762667dc68738a5f3da2b10
brotherbill <bilaltahacs@gmail.com>
2022-08-19 07:33:47 -0400

    Init NestJS Project

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: 'brobot-api',
-      script: 'dist/main.js',
-    },
-  ],
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: '3.143.76.120',
-      key: '~/.ssh/brobot-api.pem',
-      ref: 'origin/nestjs-migration',
-      repo: 'git@github.com:TahaBilalCS/brobot.git',
-      path: '/home/ubuntu/brobot',
-      'post-deploy':
-        'npm install && npm run prebuild && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-    },
-  },
+    apps: [
+        {
+            name: 'brobot-api',
+            script: 'dist/main.js'
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: '3.143.76.120',
+            key: '~/.ssh/brobot-api.pem',
+            ref: 'origin/nestjs-migration',
+            repo: 'git@github.com:TahaBilalCS/brobot.git',
+            path: '/home/ubuntu/brobot',
+            'post-deploy':
+                'npm install && npm run prebuild && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
+        }
+    }
 };
