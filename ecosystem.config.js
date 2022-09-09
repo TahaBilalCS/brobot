3176150e9204b89e0e0f5f54c0e4b27988b3c27d
TahaBilalCS <bilaltahacs@gmail.com>
2022-09-08 23:36:28 -0400

    Init Prisma with AWS RDS - NestJS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
             script: 'dist/main.js',
             env: {
                 NODE_ENV: 'development'
+            },
+            env_production: {
+                NODE_ENV: 'production'
             }
         }
     ],
