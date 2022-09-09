93998ba6500af8b9a4d777ab85e77b06cfc7e9be
TahaBilalCS <bilaltahacs@gmail.com>
2022-09-09 19:23:50 -0400

    Init Prisma with AWS RDS - NestJS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,6 +17,7 @@ export class AppController {
 
         console.log('TEST', configService.get('TEST_SECRET'));
         console.log('SUPAH', configService.get('SUPAH'));
+        console.log('SHTEST', configService.get('SHTEST'));
     }
 
     @Get('post/:id')
