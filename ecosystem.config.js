6e53fec086dce836d904b8a739b2dfc61f596523
TahaBilalCS <bilaltahacs@gmail.com>
2022-09-08 22:48:01 -0400

    Init Prisma with AWS RDS - NestJS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,10 +17,10 @@ module.exports = {
             ref: 'origin/nestjs-migration',
             repo: 'git@github.com:TahaBilalCS/brobot.git',
             path: '/home/ubuntu/brobot',
-            env: {
-                NODE_ENV: 'production',
-                TEST: process.env.TEST
-            },
+            // env: {
+            //     NODE_ENV: 'production',
+            //     TEST: process.env.TEST
+            // },
             //
             'post-deploy':
                 'npm install && npm run prebuild && npm run build && npx prisma migrate deploy && pm2 startOrRestart ecosystem.config.js --env production --update-env'
