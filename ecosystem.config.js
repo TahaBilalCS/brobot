60b51c2411c8e7c505f1dd6ad3a45846c2bf2609
TahaBilalCS <bilaltahacs@gmail.com>
2022-09-09 22:09:21 -0400

    Init Prisma with AWS RDS - NestJS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,8 @@ module.exports = {
                 NODE_ENV: 'development'
             },
             env_production: {
-                NODE_ENV: 'production'
+                NODE_ENV: 'production',
+                TEST: 'test'
             }
         }
     ],
@@ -24,7 +25,7 @@ module.exports = {
             //     NODE_ENV: 'production'
             // },
             'post-deploy':
-                'source /etc/environment && nvm use 16.17.0 && npm install && npm run prebuild && npm run build && npx prisma migrate deploy && pm2 startOrRestart ecosystem.config.js --env production --update-env'
+                'source /etc/environment && nvm use 16.17.0 && npm install && npm run prebuild && npm run build && npx prisma migrate deploy && pm2 stop brobot-api && pm2 start ecosystem.config.js --env production --update-env'
         }
     }
 };
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { PrismaService } from './prisma.service';
-import * as chokidar from 'chokidar';
-import * as childProcess from 'child_process';
+// import * as chokidar from 'chokidar';
+// import * as childProcess from 'child_process';
 
-const envWatch = () => {
-    chokidar.watch('/etc/environment').on('change', (event, path) => {
-        const testSH = childProcess.execSync('. /etc/environment && echo $TESTSH').toString();
-        console.log('ENVWATCH', testSH);
-        process.env.TESTSH = testSH;
-        console.log('WWW', process.env.TESTSH);
-        // console.log(event, path);
-    });
-};
+// const envWatch = () => {
+//     chokidar.watch('/etc/environment').on('change', (event, path) => {
+//         const testSH = childProcess.execSync('. /etc/environment && echo $TESTSH').toString();
+//         console.log('ENVWATCH', testSH);
+//         process.env.TESTSH = testSH;
+//         console.log('WWW', process.env.TESTSH);
+//         // console.log(event, path);
+//     });
+// };
 
 async function bootstrap() {
-    envWatch();
+    // envWatch();
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     const prismaService = app.get(PrismaService);
