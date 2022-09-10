e89cb9134b9da3ab6c603f5a140c05a36b1df6f7
TahaBilalCS <bilaltahacs@gmail.com>
2022-09-09 21:50:24 -0400

    Init Prisma with AWS RDS - NestJS

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { PrismaService } from './prisma.service';
+import chokidar from 'chokidar';
+import * as childProcess from 'child_process';
+
+const envWatch = () => {
+    chokidar.watch('/etc/environment').on('change', (event, path) => {
+        const testSH = childProcess.execSync('source /etc/environment && echo $TESTSH');
+        console.log('ENVWATCH', testSH);
+        // console.log(event, path);
+    });
+};
 
 async function bootstrap() {
+    envWatch();
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     const prismaService = app.get(PrismaService);
